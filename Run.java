e5a4d9cf4301523c8236b80d0fc07535533453b5
EunjungKwon <57833468+EunjungKwon@users.noreply.github.com>
2019-11-16 21:28:11 +0900

    Add files via upload

---
+++ Run.java
@@ -0,0 +1,21 @@
+
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+public class Run {
+    public static void main(String[] args) throws Exception {
+    	
+    	CharStream input = CharStreams.fromFileName("test.txt");
+  
+       // CharStream input = CharStreams.fromStream(System.in);
+        CalculatorLexer lexer = new CalculatorLexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        CalculatorParser parser = new CalculatorParser(tokens);
+        ParseTree tree = parser.input();
+          
+        System.out.println("Result: ");        
+       
+        System.out.println(tree.toStringTree(parser));
+        
+    }
+}
